6967bd7475b1695d1816b985e181123165b0f359
Ali Fatih AYDIN <bozok17@gmail.com>
2017-05-11 14:45:02 +0300

    map and slice learning

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
 
+var (
+	errorEmptyString = errors.New("Unwilling to print an emty string")
+)
+
 func printer(msg string) error {
 	if msg == "" {
-		return fmt.Errorf("Unwilling to print an emty string")
+		return errorEmptyString
 	}
 	_, err := fmt.Printf("%s\n", msg)
 	return err
