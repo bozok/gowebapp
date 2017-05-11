e6e6f0cbdf396adf762ef4a4614436cda53a2423
Ali Fatih AYDIN <bozok17@gmail.com>
2017-05-11 12:26:55 +0300

    errors training

--- main.go
+++ main.go
@@ -1,53 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+func printer(msg string) error {
+	if msg == "" {
+		return fmt.Errorf("Unwilling to print an emty string")
+	}
+	_, err := fmt.Printf("%s\n", msg)
+	return err
+}
 
 func main() {
-	dayMonths := map[string]int{
-		"Jan": 31,
-		"Feb": 28,
-		"Mar": 31,
-		"Apr": 30,
-		"May": 31,
-		"Jun": 30,
-		"Jul": 31,
-		"Aug": 31,
-		"Sep": 30,
-		"Oct": 31,
-		"Nov": 30,
-		"Dec": 31,
+	if err := printer(""); err != nil {
+		fmt.Printf("printer failed: %s\n", err)
+		os.Exit(1)
 	}
-
-	// dayMonths := make(map[string]int)
-	// dayMonths["Jan"] = 31
-	// dayMonths["Feb"] = 28
-	// dayMonths["Mar"] = 31
-	// dayMonths["Apr"] = 30
-	// dayMonths["May"] = 31
-	// dayMonths["Jun"] = 30
-	// dayMonths["Jul"] = 31
-	// dayMonths["Aug"] = 31
-	// dayMonths["Sep"] = 30
-	// dayMonths["Oct"] = 31
-	// dayMonths["Nov"] = 30
-	// dayMonths["Dec"] = 31
-
-	// days, ok := dayMonths["Jun"]
-	// if !ok {
-	// 	fmt.Printf("Can't get days for Jun")
-	// } else {
-	// 	fmt.Printf("Days in June %d\n", days)
-	// }
-
-	has28 := 1
-
-	delete(dayMonths, "Feb")
-	delete(dayMonths, "February")
-
-	for _, days := range dayMonths {
-		if days == 28 {
-			has28++
-		}
-	}
-	fmt.Printf("%d months 28 days\n", has28)
 }
