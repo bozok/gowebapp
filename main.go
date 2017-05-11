2349e3fec5b71c9a3d6da12264467cc2d7a3c52f
Ali Fatih AYDIN <bozok17@gmail.com>
2017-05-11 23:38:22 +0300

    Late night workings.

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-	"os"
 )
 
 var (
@@ -19,12 +18,33 @@ func printer(msg string) error {
 }
 
 func main() {
-	if err := printer(""); err != nil {
-		if err == errorEmptyString {
-			fmt.Printf("You tried to print an empty string!\n")
-		} else {
-			fmt.Printf("printer failed: %s\n", err)
+	dayMonths := make(map[string]int)
+	dayMonths["Jan"] = 31
+	dayMonths["Feb"] = 28
+	dayMonths["Mar"] = 31
+	dayMonths["Apr"] = 30
+	dayMonths["May"] = 31
+	dayMonths["Jun"] = 30
+	dayMonths["Jul"] = 31
+	dayMonths["Aug"] = 31
+	dayMonths["Sep"] = 30
+	dayMonths["Oct"] = 31
+	dayMonths["Nov"] = 30
+	dayMonths["Dec"] = 31
+
+	for month, day := range dayMonths {
+		//fmt.Printf("%d\n", days)
+		if month == "Feb" {
+			fmt.Printf("%d days in February\n", day)
 		}
-		os.Exit(1)
 	}
+
+	// if err := printer(""); err != nil {
+	// 	if err == errorEmptyString {
+	// 		fmt.Printf("You tried to print an empty string!\n")
+	// 	} else {
+	// 		fmt.Printf("printer failed: %s\n", err)
+	// 	}
+	// 	os.Exit(1)
+	// }
 }
